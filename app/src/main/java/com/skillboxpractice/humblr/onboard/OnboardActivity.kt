29bc6dfdf9045b2cbabc3f31ab805c58531568f7
Иван <jazzman80@yandex.ru>
2023-05-18 18:11:24 +0300

    Индикатор с точками для карусели

--- app/src/main/java/com/skillboxpractice/humblr/onboard/OnboardActivity.kt
+++ app/src/main/java/com/skillboxpractice/humblr/onboard/OnboardActivity.kt
@@ -3,6 +3,7 @@ package com.skillboxpractice.humblr.onboard
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
 import com.skillboxpractice.humblr.R
 import com.skillboxpractice.humblr.databinding.ActivityOnboardBinding
 
@@ -11,18 +12,27 @@ import com.skillboxpractice.humblr.databinding.ActivityOnboardBinding
 class OnboardActivity : AppCompatActivity() {
 
     lateinit var adapter: OnboardAdapter
+    private lateinit var binding: ActivityOnboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityOnboardBinding =
+        binding =
             DataBindingUtil.setContentView(this, R.layout.activity_onboard)
         setContentView(binding.root)
 
         binding.lifecycleOwner = this
+
+        //TODO Теперь это лишняя переменная
         binding.parentActivity = this
 
-        adapter = OnboardAdapter(this)
+        binding.viewPager.adapter = OnboardAdapter(this)
+
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager
+        ) { _, _ -> }.attach()
 
     }
+
 }
