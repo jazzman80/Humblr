67ca1ed8cb85e468ac5a8f20bfadca9adf10feea
Иван <jazzman80@yandex.ru>
2023-05-24 01:03:03 +0300

    Основная навигация

---
+++ app/src/main/java/com/skillboxpractice/humblr/main/FavoritesFragment.kt
@@ -0,0 +1,37 @@
+package com.skillboxpractice.humblr.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.skillboxpractice.humblr.R
+import com.skillboxpractice.humblr.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorites, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // binding.fragment = this
+        binding.lifecycleOwner = this
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/skillboxpractice/humblr/main/MainActivity.kt
+++ app/src/main/java/com/skillboxpractice/humblr/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.skillboxpractice.humblr.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.skillboxpractice.humblr.R
 import com.skillboxpractice.humblr.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationBar.setupWithNavController(navController)
 
     }
 
